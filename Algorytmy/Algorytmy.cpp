23e1457c0c37888bda416571cd6a09d9d13cfff2
Mateusz Galazka <mgalazka@regenersis.local>
2020-11-21 17:24:50 +0100

    Commit1

---
+++ Algorytmy/Algorytmy.cpp
@@ -0,0 +1,23 @@
+﻿// Algorytmy.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void f1(int& a) {
+	a = 15;
+}
+
+
+int main()
+{
+	cout << "Algorytmy" << endl;
+	int a = 5;
+	cout << a << endl;
+	f1(a);
+	cout << a;
+}
+
+
+
